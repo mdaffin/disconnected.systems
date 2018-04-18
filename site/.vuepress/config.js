340c0c71816402a41e969629d0ac777487ab3a89
Michael Daffin <michael@daffin.io>
2018-04-18 21:11:02 +0100

    Renames all README.md to index.md
    This is meant to be supported but they version that supports it has not been relaesed yet. It technically works just produces a bunch of warnings.

--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -1,4 +1,11 @@
 module.exports = {
   title: 'Disconnected Systems',
   description: 'Programming, Linux and all things Open Source',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Github', link: 'https://github.com/mdaffin' },
+      { text: 'Gitlab', link: 'https://gitlab.com/mdaffin' },
+    ]
+  }
 }
