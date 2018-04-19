23b0aed7246acb6df2e39a5bfe443d6d65712c1f
Michael Daffin <michael@daffin.io>
2018-04-19 20:23:21 +0100

    Adds google analytics code
    closes #10

--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   title: 'Disconnected Systems',
   description: 'Programming, Linux and all things Open Source',
+  ga: 'UA-57006875-2',
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
