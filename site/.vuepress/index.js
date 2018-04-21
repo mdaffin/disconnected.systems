fbcce6957e28ff046019242fbd56e5943ad8c7c9
Michael Daffin <michael@daffin.io>
2018-04-21 17:44:47 +0100

    Adds rss feed

--- site/.vuepress/index.js
+++ site/.vuepress/index.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const fs = require('fs-extra')
 
-module.exports = async function ({options}) {
+async function netlifyRedirects({options}) {
   const redirects = options.siteData.pages
     .filter(page => page.frontmatter.aliases)
     .map(page => page.frontmatter.aliases.map(alias => ({
@@ -17,4 +17,40 @@ module.exports = async function ({options}) {
         redirects.join('\n')
     )
   }
+}
+
+async function rssFeed({options}) {
+  var RSS = require('rss')
+  var feed = new RSS({
+    title: options.siteData.title,
+    description: options.siteData.description,
+    feed_url: "https://disconnected.systems/rss.xml",
+    site_url: "https://disconnected.systems",
+    copyright: "2018 Michael Daffin",
+    language: 'en',
+  })
+
+  options.siteData.pages
+    .filter(page => page.path.startsWith('/blog/'))
+    .map(page => ({...page, date: new Date(page.frontmatter.date)}))
+    .sort((a, b) => b.date - a.date)
+    .map(page => ({
+      title: page.title,
+      description: page.frontmatter.description,
+      url: `https://disconnected.systems${page.path}`,
+      //guid: "",
+      //categories: "",
+      date: page.date,
+    }))
+    .forEach(page => feed.item(page))
+
+  await fs.writeFile(
+    path.resolve(options.outDir, 'rss.xml'),
+    feed.xml()
+  )
+}
+
+module.exports = async function (args) {
+  await netlifyRedirects(args)
+  await rssFeed(args)
 }
