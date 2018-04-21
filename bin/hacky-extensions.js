7d18a6ed2c92b161704c3a4fa0cce6f9ae74dbb4
Michael Daffin <michael@daffin.io>
2018-04-21 16:59:33 +0100

    Changes to netlify redirects

--- bin/hacky-extensions.js
+++ bin/hacky-extensions.js
@@ -1,26 +1,13 @@
 #!/usr/bin/env node
-const path = require('path')
-const fs = require('fs-extra')
-
-async function writeRedirects(options) {
-    const redirects = options.siteData.pages
-        .filter(page => page.frontmatter.aliases)
-        .map(page => page.frontmatter.aliases.map(alias => ({
-                dest: page.path,
-                alias: alias
-            })))
-        .reduce((acc, aliases) => acc.concat(aliases), [])
-        .map(({dest, alias}) => `${alias}\t${dest}`)
-        .join('\n')
-        
-    await fs.writeFile(path.resolve(options.outDir, '_redirects'), redirects)
-};
-
 async function main() {
-    const prepare = require('../node_modules/vuepress/lib/prepare')
-    const options = await prepare('site')
-    
-    await writeRedirects(options)
+  const path = require('path')
+  const fs = require('fs-extra')
+  const plugin = require('../site/.vuepress')
+
+  const prepare = require('../node_modules/vuepress/lib/prepare')
+  const options = await prepare('site')
+
+  await plugin({options})
 }
 
 main()
