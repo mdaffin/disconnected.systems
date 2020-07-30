d80502b9c92dbb66741964a0cb379a4ac635f2ce
Michael Daffin <michael@daffin.io>
2020-07-30 01:07:02 +0100

    Adds inital support for reading html and markdown files

--- src/transform.rs
+++ src/transform.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use std::fs::{create_dir_all, read, write};
+use std::fs::{create_dir_all, read, read_to_string, write};
 use std::path::{Path, PathBuf};
 
 #[derive(Debug)]
@@ -21,7 +21,7 @@ pub enum Content {
 pub struct HtmlContent {
     route: PathBuf,
     collection: Option<String>,
-    layout: String,
+    layout: Option<String>,
     content: String,
 }
 
@@ -33,10 +33,20 @@ pub struct WritableContent {
 
 impl SourcePage {
     pub fn read(self) -> Result<Content> {
-        Ok(Content::Raw(WritableContent {
-            route: self.route,
-            content: read(self.source)?,
-        }))
+        Ok(match self.source.extension() {
+            Some(extension) if extension == "html" || extension == "md" => {
+                Content::Html(HtmlContent {
+                    route: self.route,
+                    collection: None,
+                    layout: None,
+                    content: read_to_string(self.source)?,
+                })
+            }
+            _ => Content::Raw(WritableContent {
+                route: self.route,
+                content: read(self.source)?,
+            }),
+        })
     }
 }
 
@@ -63,7 +73,7 @@ mod tests {
     #[test_case("png" ; "png files")]
     #[test_case("jpeg" ; "jpeg files")]
     #[test_case("jpg" ; "jpg files")]
-    fn content_is_unmodified_when_source_is_read(extension: &str) {
+    fn content_is_unmodified_when_source_is_read_and_produces_raw_content(extension: &str) {
         let temp_dir = tempfile::tempdir().unwrap();
         let content = b"content";
 
@@ -94,5 +104,55 @@ mod tests {
 
         let dest_path = temp_dir.path().join(page.route);
         assert_eq!(read(dest_path).unwrap(), page.content);
+    }
+
+    #[test_case("html" ; "html files")]
+    #[test_case("md" ; "markdown files")]
+    fn content_is_unmodified_when_source_is_read_and_produces_html_content(extension: &str) {
+        let temp_dir = tempfile::tempdir().unwrap();
+        let content = "<h1>content</h1>";
+
+        let page = SourcePage {
+            source: temp_dir.path().join(format!("main.{}", extension)),
+            route: PathBuf::from(""),
+        };
+        write(&page.source, content).unwrap();
+
+        match page.read().unwrap() {
+            Content::Html(html) => assert_eq!(html.content, content),
+            Content::Raw(_) => panic!("raw content returned"),
+        }
+    }
+
+    #[test_case("main.css" ; "css file")]
+    #[test_case("section/main.css" ; "css file in directory")]
+    #[test_case("section/subsection/main.css" ; "css file in nested directory")]
+    #[test_case("main.jpg" ; "jpg file")]
+    #[test_case("section/main.jpg" ; "jpg file in directory")]
+    #[test_case("section/subsection/main.jpg" ; "jpg file in nested directory")]
+    #[test_case("main.md" ; "md file")]
+    #[test_case("section/main.md" ; "md file in directory")]
+    #[test_case("section/subsection/main.md" ; "md file in nested directory")]
+    #[test_case("main.html" ; "html file")]
+    #[test_case("section/main.html" ; "html file in directory")]
+    #[test_case("section/subsection/main.html" ; "html file in nested directory")]
+    fn route_from_sorce_is_unmodified_when_converted_to_content(route: &str) {
+        let temp_dir = tempfile::tempdir().unwrap();
+        let content = b"content";
+        let route = PathBuf::from(route);
+
+        let page = SourcePage {
+            source: temp_dir.path().join(route.file_name().unwrap()),
+            route: route.clone(),
+        };
+        write(&page.source, content).unwrap();
+
+        assert_eq!(
+            match page.read().unwrap() {
+                Content::Html(html) => html.route,
+                Content::Raw(raw) => raw.route,
+            },
+            route
+        );
     }
 }
